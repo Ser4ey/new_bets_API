6fd1e735892ff8d14da3e5a4d1c4b22aa8d71de2
ser4 <pristup2040@gmail.com>
2021-10-22 18:33:50 +1000

    update start

--- start.py
+++ start.py
@@ -15,34 +15,56 @@ def make_bet_multipotok(All_elements_array):
         print('Реанимация аккаунта')
         driver.reanimaite_bet365com()
 
+
 def reanimate_bet365com(driver):
     driver.reanimaite_bet365com()
 
 
+def register_bet365_multipotok(AccountData):
+    driver2, login, password = AccountData[0], AccountData[1], AccountData[2]
+    print(f'Вход в аккаунт: {login}')
+
+    while True:
+        r = driver2.open_bet365com()
+        if r is None:
+            break
+        else:
+            print('Сайт bet365 не загрузился')
+            driver2.restart_driver()
+            time.sleep(5)
+
+    try:
+        driver2.log_in_bet365(login, password)
+    except:
+        print('-'*100)
+        print(f'Не удалось войти в аккаунт {login}')
+        print('-'*100)
+
+
 driverParimatch = FireFoxForPimatch()
 
-List_of_bet_account = [0] * len(AccountsBet365)
+list_of_start_info = []
+List_of_bet_account = []
+
 i1 = 1
 for i in range(len(AccountsBet365)):
     print(f'Запуск аккаунта {i1}')
     account_data = AccountsBet365[i]
-    # запускаем аккаунт пока он не запустится
-    while True:
-        try:
-            driver2 = FireFoxDriverMain(account_data['bet_value'])
-            driver2.log_in_bet365(login=account_data['bet365_login'], password=account_data['bet365_password'])
-            List_of_bet_account[i] = driver2
-            break
-        except:
-            print('Ошибка при запуске аккаунта')
-            print(f'Повторный запуск аккаунта {i1}')
-            try:
-                driver2.driver.close()
-                driver2.driver.quit()
-            except:
-                pass
+
+    driver2 = FireFoxDriverMain(account_data['bet_value'])
+    List_of_bet_account.append(driver2)
+    start_info = [driver2, account_data['bet365_login'], account_data['bet365_password']]
+
+    list_of_start_info.append(start_info)
 
     i1 += 1
+
+
+with Pool(processes=len(list_of_start_info)) as p:
+    p.map(register_bet365_multipotok, list_of_start_info)
+
+
+# начало программы
 AllBetsSet = set()
 
 reboot_counter = 0
