66d1011e67ac4e921b0ea5b9b6bfb8f9588fd913
ser4 <pristup2040@gmail.com>
2021-10-29 22:50:34 +1000

    del test from API

--- API_worker20.py
+++ API_worker20.py
@@ -255,9 +255,9 @@ def get_fork_from_API(AllBetsSet):
 
     return 'NO', {}
 
-
-A = set()
-for i in range(100):
-    time.sleep(3)
-    a, b = get_fork_from_API(A)
-    print(a, b)
+#
+# A = set()
+# for i in range(100):
+#     time.sleep(3)
+#     a, b = get_fork_from_API(A)
+#     print(a, b)
